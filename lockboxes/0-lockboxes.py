ea111dcb281a3dce4172dbe25da01c63572dec23
ayaaribi123 <ayaaribi11@gmail.com>
2024-01-24 01:52:11 +0100

    minoperations

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,12 +3,13 @@
 Lockboxes
 """
 
+
 def canUnlockAll(boxes):
     """
     check if all the boxes can be opened.
     """
-    i = len(boxes)
-    open = [False] * i
+
+    open = [False] * len(boxes)
     open[0] = True
     Res = [0]
 
@@ -17,7 +18,7 @@ def canUnlockAll(boxes):
         current_box = Res.pop(0)
 
         for key in boxes[current_box]:
-            if 0 <= key < i and not open[key]:
+            if 0 <= key < len(boxes) and not open[key]:
                 open[key] = True
                 Res.append(key)
 
