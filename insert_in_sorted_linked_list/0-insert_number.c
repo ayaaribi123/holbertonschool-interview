9137985e3bd77104ecd45c98375455b237cbe38b
ayaaribi123 <ayaaribi11@gmail.com>
2024-01-24 12:17:08 +0100

    linked list

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -1,35 +1,38 @@
 #include "lists.h"
-
 /**
- * insert_node - inserts a number into a sorted singly linked list
+ * insert_node - function that insert a number into a sorted singly linked list
  * @head: head list
  * @number: node to insert
  * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *newNode = malloc(sizeof(listint_t));
-				listint_t *prevNode = *head;
-    if (!newNode)
-        return NULL;
+    listint_t *newNode;
+    listint_t *currentNode;
 
-    newNode->n = number;
-    newNode->next = NULL;
-
-    if (*head == NULL || (*head)->n >= newNode->n)
+    currentNode = *head;
+    newNode = malloc(sizeof(listint_t));
+    if (newNode)
     {
-        newNode->next = *head;
-        *head = newNode;
-        return newNode;
+        newNode->n = number;
+        newNode->next =NULL;
+
+        if (*head == NULL || (*head)->n >= newNode->n)
+        {
+            newNode->next = *head;
+            *head = newNode;
+        }
+        else
+        {
+            while (currentNode->next && currentNode->next->n < newNode->n)
+			{
+				currentNode = currentNode->next;
+			}
+			newNode->next = currentNode->next;
+			currentNode->next = newNode;
+        }
+        return (newNode);
+        
     }
-
-    while (prevNode->next && prevNode->next->n < newNode->n)
-    {
-        prevNode = prevNode->next;
-    }
-
-    newNode->next = prevNode->next;
-    prevNode->next = newNode;
-
-    return newNode;
+    return (NULL);
 }
